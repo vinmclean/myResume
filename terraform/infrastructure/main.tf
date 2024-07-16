3e172d02dfc340b6ebe595cbeae020e3d02ada55
vinmclean <mclean.vincent2@gmail.com>
2024-07-16 11:34:34 -0400

    TF isolate - create new function app

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -1,6 +1,11 @@
+resource "random_string" "random" {
+  length  = 8
+  special = false
+  upper   = false
+}
 
 resource "azurerm_resource_group" "fun_app_rg" {
-  name     = var.resource_group_name_func_app
+  name     = "${var.resource_group_name_func_app}-${random_string.random.result}-rg"
   location = "eastus2"
 }
 
@@ -38,7 +43,7 @@ resource "azurerm_cosmosdb_sql_database" "database" {
 }
 
 resource "azurerm_storage_account" "func_app_storage_account" {
-  name                     = var.storage_account_name
+  name                     = "${var.storage_account_name}${random_string.random.result}"
   resource_group_name      = azurerm_resource_group.fun_app_rg.name
   location                 = "eastus"
   account_tier             = "Standard"
@@ -46,34 +51,23 @@ resource "azurerm_storage_account" "func_app_storage_account" {
 }
 
 resource "azurerm_service_plan" "app_plan" {
-  name                = var.app_service_plan_name
+  name                = "${var.app_service_plan_name}-${random_string.random.result}"
   location            = "eastus"
   resource_group_name = azurerm_resource_group.fun_app_rg.name
-
-
-  sku_name = "Y1"
-  os_type  = "Linux"
+  sku_name            = "Y1"
+  os_type             = "Linux"
 }
 
 resource "azurerm_linux_function_app" "py_func_app" {
-  name                       = var.function_app_name
+  name                       = "${var.function_app_name}-${random_string.random.result}"
   location                   = var.location
   resource_group_name        = azurerm_resource_group.fun_app_rg.name
   service_plan_id            = azurerm_service_plan.app_plan.id
   storage_account_name       = azurerm_storage_account.func_app_storage_account.name
   storage_account_access_key = azurerm_storage_account.func_app_storage_account.primary_access_key
 
-  app_settings = {
-    "APPINSIGHTS_INSTRUMENTATIONKEY"        = "74c873fd-0b5a-4938-bab7-2d39d91e62d1"
-    "APPLICATIONINSIGHTS_CONNECTION_STRING" = "InstrumentationKey=74c873fd-0b5a-4938-bab7-2d39d91e62d1;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/"
-    "AzureCosmosDBConnectionString"         = var.cosmosdb_connection_string
-    "ENABLE_ORYX_BUILD"                     = "1"
-    "AzureWebJobsFeatureFlags"              = "EnableWorkerIndexing"
-    "FUNCTIONS_WORKER_RUNTIME"              = "python"
-    "SCM_DO_BUILD_DURING_DEPLOYMENT"        = "1"
-  }
-
   site_config {
+
     cors {
       allowed_origins = [
         "https://portal.azure.com",
@@ -81,5 +75,11 @@ resource "azurerm_linux_function_app" "py_func_app" {
       ]
       support_credentials = true
     }
+  }
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"      = "python"
+    "FUNCTIONS_EXTENSION_VERSION"   = "~4" # Specify the Functions runtime version
+    "AzureCosmosDBConnectionString" = var.cosmosdb_connection_string
   }
 }
